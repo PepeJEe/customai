f508e6656b2130303f957080fd26668303bd7e4b
Perttu Vierimaa <perttu.vierimaa@gmail.com>
2026-04-19 03:14:04 +0300

    fixed log folder

--- llm.py
+++ llm.py
@@ -1,17 +1,27 @@
 import httpx
 from config import MODEL_NAME, SYSTEM_PROMPT, OLLAMA_URL
+from logger import logger
+
 async def receive_request(messages):
-    full_messages = [{"role": "system", "content": SYSTEM_PROMPT}] + messages
-    
-    async with httpx.AsyncClient(timeout=300.0) as client:
-        response = await client.post(
-            OLLAMA_URL,
-            json={
-                "model": MODEL_NAME,
-                "messages": full_messages,
-                "stream": False
-            }
+    try:
+        logger.info("Sending request to Ollama")
+        full_messages = [{"role": "system", "content": SYSTEM_PROMPT}] + messages
+        
+        async with httpx.AsyncClient(timeout=300.0) as client:
+            response = await client.post(
+                OLLAMA_URL,
+                json={
+                    "model": MODEL_NAME,
+                    "messages": full_messages,
+                    "stream": False
+                }
             )
 
-        result = response.json()
+            result = response.json()
+            logger.info("Got response from Ollama: {content[:50]}")
+
         return result["message"]["content"]
+    
+    except Exception:
+        logger.info("Failed to get response from Ollama")
+        raise
--- logger.py
+++ logger.py
@@ -1,7 +1,10 @@
 import logging
 from logging.handlers import RotatingFileHandler
+import os
 
-logger = logging.getLogger("__name__")
+os.makedirs("logs", exist_ok=True)
+
+logger = logging.getLogger("customai")
 logger.setLevel(level=logging.INFO)
 
 console_handler = logging.StreamHandler()
@@ -15,3 +18,9 @@ file_handler = RotatingFileHandler(
 file_handler.setLevel(logging.INFO)
 
 formatter = logging.Formatter("%(asctime)s | %(levelname)8s | %(filename)s - %(lineno)8d | %(message)s")
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
